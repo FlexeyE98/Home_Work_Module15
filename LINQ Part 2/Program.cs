6a8f2e0ee5e7bbe0507cf13a67c2d9e211c5acc1
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-06-30 23:14:27 +0300

    Итоговое задание 15 модуля

---
+++ LINQ Part 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+           var allStudents =  GetAllStudents(classes);
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+        static string[] GetAllStudents(Classroom[] classes) //либо я не понял задания, либо very easy вышло :)
+        {
+            var allStudents = from student in classes
+                              from stud in student.Students
+                              select stud;
+            return allStudents.ToArray();
+        }
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
